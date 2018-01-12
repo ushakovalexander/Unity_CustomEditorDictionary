c339750a0d149eefc2ba006c9552d8cf514e3be6
Alexander Ushakov <ushakov@renatus.com>
2018-01-12 21:23:59 +0200

    Initial commit

---
+++ Assets/Scripts/ScriptableUserDataEditor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ScriptableUserData))]
+public class ScriptableUserDataEditor : Editor {
+
+	private SerializedObject _serializedUserData;
+	private ScriptableUserData _userData;
+
+  public override void OnInspectorGUI() {
+
+		SerializedProperty userNameProperty = SerializedUserData.FindProperty(GetMemberName(() => UserData.userName));
+		SerializedProperty userIdProperty = SerializedUserData.FindProperty(GetMemberName(() => UserData.userId));
+		SerializedProperty purchasesProperty = SerializedUserData.FindProperty(GetMemberName(() => UserData.purchases));
+
+		EditorGUILayout.PropertyField(userNameProperty);
+		EditorGUILayout.PropertyField(userIdProperty);
+
+		SerializableDictionary<string, int> dict = UserData.purchases;
+		foreach(KeyValuePair<string, int> kvp in dict) {
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.TextField(kvp.Key);
+			EditorGUILayout.TextField(kvp.Value.ToString());
+			EditorGUILayout.EndHorizontal();
+		}
+
+		if(dict.Keys.Count < 3) {
+			string newKey = UnityEngine.Random.Range(0, 10).ToString();
+			if(!dict.ContainsKey(newKey)) {
+				int newValue = UnityEngine.Random.Range(0, 10);
+				dict.Add(newKey, newValue);
+			}
+		}
+
+		SerializedUserData.ApplyModifiedProperties();
+	}
+
+	private SerializedObject SerializedUserData {
+		get {
+			if(_serializedUserData == null) {
+				_serializedUserData = new SerializedObject(target);
+			}
+			return _serializedUserData;
+		}
+	}
+
+  public ScriptableUserData UserData {
+		get {
+			if(_userData == null) {
+				_userData = (ScriptableUserData) target;
+			}
+			return _userData;
+		}
+	}
+
+  private string GetMemberName<TValue>(Expression<Func<TValue>> memberAccess) {
+		return ((MemberExpression)memberAccess.Body).Member.Name;
+	}
+}
---
+++ Assets/Scripts/SerializableDictionary/SerializableDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver {
+	[SerializeField]
+	private List<TKey> keys = new List<TKey>();
+
+	[SerializeField]
+	private List<TValue> values = new List<TValue>();
+
+  public void OnBeforeSerialize() {
+		keys.Clear();
+		values.Clear();
+		foreach (KeyValuePair<TKey, TValue> data in this) {
+			keys.Add(data.Key);
+			values.Add(data.Value);
+		}
+  }
+
+  public void OnAfterDeserialize() {
+		Clear();
+		if(keys.Count != values.Count) {
+			throw new Exception("Invalid data");
+		}
+		for (int i = 0; i < keys.Count; i++) {
+			Add(keys[i], values[i]);
+		}
+  }
+}
